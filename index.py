d7e843e4356376d2442d0e113f75428f61402ef1
ashish <ashishjain1547@gmail.com>
2024-03-11 20:16:16 +0530

    #

---
+++ index.py
@@ -0,0 +1,55 @@
+from flask import Flask, redirect, render_template,request,url_for
+
+import json, requests
+
+from flask_cors import CORS, cross_origin
+
+from flask import Response, jsonify
+
+import sqlite3
+
+app=Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template("index.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    print(request.data)
+    username = ""
+    password = ""
+    retypepassword = ""
+    if request.method=="POST":
+        username=request.form["personname"]
+        password=request.form["personpassword"]
+        retypepassword=request.form["personretypepassword"]
+        print(username,password,retypepassword)
+    # return render_template("result.html", username=username, password=password, retypepassword=retypepassword)
+
+    
+
+    conn = sqlite3.connect('sharks.db')
+
+    # Creating a cursor object using the  
+    # cursor() method 
+    cursor = conn.cursor() 
+
+    print('Opened database successfully')
+
+    # Creating table 
+    table ="""CREATE TABLE IF NOT EXISTS USER_INFO (username VARCHAR(255), password VARCHAR(255))"""
+    cursor.execute(table) 
+    
+    # Queries to INSERT records. 
+    cursor.execute("INSERT INTO USER_INFO VALUES ('" + username + "', '" + password + "')") 
+        
+    conn.close()
+
+
+    return "Hi, " +  username
+    # Redirect to the homepage if accessed via GET method
+    
+if __name__=='__main__':
+    app.run(debug=True)
